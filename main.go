fddbf47fcb9c17a47a2e62aeb6a8241b3b0e2ef3
Alexandra Soboleva <asoboleva@logstream.ru>
2019-02-24 18:44:09 +0300

    add twitter_2

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/dghubble/go-twitter/twitter"
+	"github.com/dghubble/oauth1"
+)
 
+func main() {
+	config := oauth1.NewConfig("consumerKey", "consumerSecret")
+	token := oauth1.NewToken("accessToken", "accessSecret")
+	httpClient := config.Client(oauth1.NoContext, token)
+
+	// Twitter client
+	client := twitter.NewClient(httpClient)
+
+	// Home Timeline
+	tweets, resp, err := client.Timelines.HomeTimeline(&twitter.HomeTimelineParams{
+		Count: 20,
+	})
+	fmt.Println(tweets, resp, err)
 }
